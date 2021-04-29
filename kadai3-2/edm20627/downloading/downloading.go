556e4f05a0de121c21f0f1ea46d1efb5795d971c
edm20627 <eng00627@gmail.com>
2021-04-30 00:46:07 +0900

    エラーハンドリング修正

--- kadai3-2/edm20627/downloading/downloading.go
+++ kadai3-2/edm20627/downloading/downloading.go
@@ -1,15 +1,16 @@
 package downloading
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/edm20627/gopherdojo-studyroom/kadai3-2/edm20627/option"
+	"golang.org/x/sync/errgroup"
 )
 
 type Download struct {
@@ -25,40 +26,49 @@ func New() *Download {
 }
 
 func (d *Download) Run() int {
+	bc := context.Background()
+	ctx, cancel := context.WithTimeout(bc, d.options.Timeout)
+	defer cancel()
 
 	// contentLengthを取得
-	contentLength, err := d.getContentLength()
+	contentLength, err := d.getContentLength(ctx)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return 1
 	}
 
 	// ダウンロード先を作成
 	dir, err := ioutil.TempDir("", "download")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return 1
 	}
 	defer os.RemoveAll(dir)
 
 	// ダウンロード
-	err = d.download(contentLength, dir)
+	err = d.download(ctx, contentLength, dir)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return 1
 	}
 
 	// マージ
 	err = d.merge(dir)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		return 1
 	}
 
 	return 0
 }
 
-func (d *Download) getContentLength() (int, error) {
+func (d *Download) getContentLength(ctx context.Context) (int, error) {
 	req, err := http.NewRequest("HEAD", d.options.URL, nil)
 	if err != nil {
 		return 0, err
 	}
+
+	req = req.WithContext(ctx)
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -74,73 +84,103 @@ func (d *Download) getContentLength() (int, error) {
 	}
 }
 
-func (d *Download) download(contentLength int, dir string) error {
-	preMin := 0
-	min, max := 0, 0
+func (d *Download) download(ctx context.Context, contentLength int, dir string) error {
+	var preMin, min, max int
+	errCh := make(chan error)
 
-	var wg sync.WaitGroup
-	wg.Add(d.options.ParallelNum)
+	g, ctx := errgroup.WithContext(ctx)
 
 	for n := d.options.ParallelNum; 0 < n; n-- {
+		n := n
 		min = preMin
 		max = contentLength/n - 1
 		preMin = contentLength / n
-		go d.parallelDownload(n, min, max, dir, &wg)
+		go d.parallelDownload(ctx, n, min, max, dir, errCh)
 	}
 
-	wg.Wait()
+	for n := d.options.ParallelNum; 0 < n; n-- {
+		g.Go(func() error {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case err := <-errCh:
+				return err
+			}
+		})
+	}
+
+	// for n := d.options.ParallelNum; 0 < n; n-- {
+	// 	n := n
+	// 	g.Go(func() error {
+	// 		min = preMin
+	// 		max = contentLength/n - 1
+	// 		preMin = contentLength / n
+	// 		return d.parallelDownload(ctx, n, min, max, dir)
+
+	// 	})
+	// }
+
+	if err := g.Wait(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (d *Download) parallelDownload(n, min, max int, dir string, wg *sync.WaitGroup) error {
-	defer wg.Done()
-
+func (d *Download) parallelDownload(ctx context.Context, n, min, max int, dir string, errCh chan error) {
 	fmt.Println(min, max)
 
 	req, err := http.NewRequest("GET", d.options.URL, nil)
 	if err != nil {
-		return err
+		errCh <- err
+		return
 	}
 
 	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", min, max))
 
+	req = req.WithContext(ctx)
+
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		errCh <- err
+		return
 	}
 
 	defer res.Body.Close()
 
 	file, err := os.Create(fmt.Sprintf("%v/%v-%v", dir, n, d.options.Output))
 	fmt.Println(file.Name())
+
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		errCh <- err
+		return
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, res.Body)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		errCh <- err
+		return
 	}
 
-	return nil
+	errCh <- nil
 }
 
 func (d *Download) merge(dir string) error {
 	file, err := os.Create(d.options.Output)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		return err
 	}
 	defer file.Close()
 
 	for n := d.options.ParallelNum; 0 < n; n-- {
 		src, err := os.Open(fmt.Sprintf("%v/%v-%v", dir, n, d.options.Output))
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
 		_, err = io.Copy(file, src)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			return err
 		}
 	}
 	return nil
--- kadai3-2/edm20627/go.mod
+++ kadai3-2/edm20627/go.mod
@@ -1,3 +1,5 @@
 module github.com/edm20627/gopherdojo-studyroom/kadai3-2/edm20627
 
 go 1.15
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
